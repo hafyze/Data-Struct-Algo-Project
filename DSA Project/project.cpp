a3beb7e9f9d45f1a3717c6a0400fdffe8d9882f6
hafyze <84822836+hafyze@users.noreply.github.com>
2022-12-28 12:23:49 +0800

    Update

--- DSA Project/project.cpp
+++ DSA Project/project.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
 
 int main(){
-    
+std::ifstream input_file("text_input.txt");
+std::string line;
+
+while (std::getline(input_file, line)) {
+    std::cout << line << std::endl;
+}
+input_file.close();
+
+return 0;
 }
