0152ca55e5ba1fe84057aa9aaa7f88a9626cc0de
hafyze <84822836+hafyze@users.noreply.github.com>
2022-12-28 20:58:28 +0800

    Update

--- DSA Project/project.cpp
+++ DSA Project/project.cpp
@@ -10,14 +10,15 @@ const int SIZE = 100;
 struct ListNode{
     int data;
     ListNode* next;
+
 };
 
 void sortLinkedList(vector<int>& number, int size){               //  Function to sort link list  //
 
-    for(int i = 0; i < size - 1; i++){                           //Loop entire array
+    for(int i = 0; i < size - 1; i++){                           // Loop entire array
         int min = i;
 
-        for(int j = i+1; j < size; j++){                         //Loops the to find minimum element
+        for(int j = i+1; j < size; j++){                         // Loops the to find minimum element
             if(number[j] < number[min]) {
                 min = j;
             }
@@ -42,26 +43,26 @@ void displaySortedLinkedList(vector<int>& number, int size){    //  DISPLAY SORT
 int main(){                                                     //  MAIN SECTION   //
 
     vector<int> number;
-    ifstream input_file("text_input.txt");                      //Open and reads the file
+    ifstream input_file("text_input.txt");                      // Open and reads the file
 
     int i;
     while (input_file >> i) {                  
-        number.push_back(i);                                    //Using vector function to declare variable in respective index
+        number.push_back(i);                                    // Using vector function to declare variable in respective index
     }
 
-    int size = number.size();                                   //Storing the size of array tu variable
-    for(i=0; i<number.size(); i++){                             //Loop to display declared variable received by the file
+    int size = number.size();                                   // Storing the size of array tu variable
+    for(i=0; i<number.size(); i++){                             // Loop to display declared variable received by the file
         cout << number[i] << endl;
 
         if(number[i] == 0){
             break;
         }
     }
-    input_file.close();                                         //Closes the file
+    input_file.close();                                         // Closes the file
 
-    sortLinkedList(number, size);                               //Call function to sort linked list
-    displaySortedLinkedList(number, size);                      //Call function to display sorted linked list
+    sortLinkedList(number, size);                               // Call function to sort linked list
+    displaySortedLinkedList(number, size);                      // Call function to display sorted linked list
     
-
+    
     return 0;
 }                                                               //               END                  //
