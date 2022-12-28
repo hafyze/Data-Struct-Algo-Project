e1f5d1d60801e899bd5b09ffa04e2f1c732133db
hafyze <84822836+hafyze@users.noreply.github.com>
2022-12-28 12:37:14 +0800

    Update

--- DSA Project/project.cpp
+++ DSA Project/project.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 const int SIZE = 100;
@@ -11,11 +13,15 @@ struct ListNode{
 };
 
 int main(){
+    vector<int> number;
     ifstream input_file("text_input.txt");
     string line;
 
-    while (getline(input_file, line)) {
-        cout << line << endl;
+    cout << "Size: " << SIZE << endl;
+
+    int i;
+    while (input_file >> i) {
+        number.push_back(i);
     }
     input_file.close();
 
